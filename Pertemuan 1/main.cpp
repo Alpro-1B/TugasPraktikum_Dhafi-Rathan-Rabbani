c491bf778a317611cdbf294b574aaff81453b601
DhafiRathan <dhafirattanr@gmail.com>
2023-10-23 06:14:17 +0700

    pertemuan 1

---
+++ Pertemuan 1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+
+    cout << "................................................" << endl;
+    cout << "Dhafi Rathan Rabbani" << endl << endl;
+    cout << "NPM : 2310631170074" << endl <<endl;
+    cout << "Kelas : 1-B  Informatika" << endl << endl;
+    cout << "Cita-cita : programmer" << endl << endl;
+    cout << "Motivasi masuk Informatika : Ingin bikin game" << endl;
+    cout << "................................................" << endl;
+    return 0;
+}
